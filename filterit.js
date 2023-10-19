d5f0cb968daf9ef78953752cd3bc67c31b8ad295
Ilmo Koste <ilmokoste@dhcp-84-241.eduroam.aalto.fi>
2023-10-19 14:16:09 +0300

    filterin update

--- filterit.js
+++ filterit.js
@@ -9,16 +9,49 @@ function createFilterClause(filterdata, column) {
     return '';
 }
 
-function myFilterFunction(filterdata, data) {
+function createBinaryClause(filterdata, column) {
+    if (filterdata[0][column].length > 0) {
+        var binaryValue = filterdata[0][column][0];
+        if (binaryValue == 0 || binaryValue == 1) {
+            return `${column} = ${binaryValue}`;
+        }
+    }
+    return '';
+}
+
+
+function FilterFunction(filterdata, data) {
     var level = filterdata[0].Level;
-    var levelsClause = createFilterClause(filterdata, 'Level');
+
+    if (level != null) {
+        if(level === 'U'){
+            var levelsClause = `UG = 1`;
+        } else if (level === 'G') {
+            var levelsClause = `G = 1`;
+        }
+    }
     var regionClause = createFilterClause(filterdata, 'Region');
+    var countryClause = createFilterClause(filterdata, 'Country')
+    var priceClause = createFilterClause(filterdata, 'Price')
+    var safetyClause = createFilterClause(filterdata, 'Safety')
+    var gpaClause = createBinaryClause(filterdata, 'Gpa'); 
+    var populationLower = filterdata[0].Population_Lower[0];
+    var populationUpper = filterdata[0].Population_Upper[0];
+    var populationClause = '';
+    if (populationLower != null && populationUpper != null) {
+        populationClause = `population >= ${populationLower} AND population <= ${populationUpper}`;
+    } else if (populationLower != null) {
+        populationClause = `population >= ${populationLower}`;
+    } else if (populationUpper != null) {
+        populationClause = `population <= ${populationUpper}`;
+    }
+
     var startMonthClause = '';
     var endMonthClause = '';
 
-    if (level.length === 2) {
+    if (level.length === 0) {
         startMonthClause = `(U_Start_Month IN ${createFilterClause(filterdata, 'Start_Month')} OR G_Start_Month IN ${createFilterClause(filterdata, 'Start_Month')})`;
-        startMonthClause = `(U_End_Month IN ${createFilterClause(filterdata, 'End_Month')} OR G_End_Month IN ${createFilterClause(filterdata, 'End_Month')})`
+        endMonthClause = `(U_End_Month IN ${createFilterClause(filterdata, 'End_Month')} OR G_End_Month IN ${createFilterClause(filterdata, 'End_Month')})`
         languageClause = filterdata[0].Language.map(lang => `(${lang}_UG = 1 OR ${lang}_G = 1)`).join(' AND ');
     } else if (level[0] === 'U') {
         startMonthClause = `U_Start_Month IN ${createFilterClause(filterdata, 'Start_Month')}`;
@@ -31,14 +64,25 @@ function myFilterFunction(filterdata, data) {
 
     }
 
+    var clauses = [];
+    
+    if (levelsClause.length > 0) clauses.push(levelsClause);
+    if (regionClause.length > 0) clauses.push(regionClause);
+    if (countryClause.length > 0) clauses.push(countryClause);
+    if (priceClause.length > 0) clauses.push(priceClause);
+    if (safetyClause.length > 0) clauses.push(safetyClause);
+    if (gpaClause.length > 0) clauses.push(gpaClause);
+    if (populationClause.length > 0) clauses.push(populationClause);
+    if (startMonthClause.length > 0) clauses.push(startMonthClause);
+    if (endMonthClause.length > 0) clauses.push(endMonthClause);
+    if (languageClause.length > 0) clauses.push(languageClause);
+    
+    var whereClause = clauses.length > 0 ? `WHERE 1=1 AND ${clauses.join(' AND ')}` : 'WHERE 1=1';
+
     var query = `
         SELECT *
         FROM ?
-        WHERE 1=1
-            AND ${levelsClause}
-            AND ${regionClause}
-            AND ${startMonthClause}
-            AND ${endMonthClause}
+        ${whereClause}
     `;
 
     var result = alasql(query, [data]);
@@ -47,8 +91,4 @@ function myFilterFunction(filterdata, data) {
 }
 
 
-
-var filterdata = [
-    {Level: ['U'], Region: ['Asia', 'Europe'], Start_Month: [], End_Month: []},
-];
 
